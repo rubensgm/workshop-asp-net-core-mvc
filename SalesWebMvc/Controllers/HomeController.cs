395d0eaca42d8d8cc6ac67538cf2cf658774e371
Rubens Marques <rubensgm@hotmail.com>
2019-08-22 09:13:58 -0300

    First controller and razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Projeto de estudo, usando C#.";
+            ViewData["email"] = "rubensgm@hotmail.com";
 
             return View();
         }
